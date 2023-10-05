69f51f2f1b3eed9b11805dfc4385849c20e492ab
SumanBalayar <orthohydrogen123@gmail.com>
2023-10-05 19:19:25 +0545

    final

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,48 +1,75 @@
 import React from 'react'
 import '../App.css'
-import {portfolio} from '../infotext/portfolio.js'
+import { portfolio,counsel,why } from '../infotext/portfolio.js'
 import logo from '../image/doctors.png'
 
 const Home = () => {
   return (
     <div>
-    <div className='home'>
-      <div className='main'>
-        <span className='headline'>Health Assistance Program</span>
-        <p className='paragraph'>A digital health program to increase the health awareness</p>
-        <ul>
-          {portfolio.map((item)=>(
-            <li className='bullets'>{item.info}</li>
-          ))}
-        </ul>
-        <div className='butt'>
-        <button className='button'>CONTACT US NOW</button>
+      <div className='home'>
+        <div className='main'>
+          <span className='headline'>Health Assistance Program</span>
+          <p className='paragraph'>A digital health program to increase the health awareness</p>
+          <ul>
+            {portfolio.map((item) => (
+              <li className='bullets'>{item.info}</li>
+            ))}
+          </ul>
+          <div className='butt'>
+            <button className='button'>CONTACT US NOW</button>
+          </div>
+        </div>
+        <div>
+          <img src={logo} alt="health image" className='custom-image' />
         </div>
       </div>
-      <div>
-        <img src={logo} alt="health image" className='custom-image'/>
+      <div className='home'>
+        <div>
+          <img src={logo} alt='Team Image' className='custom-image' />
+        </div>
+        <div className='main'>
+          <span className='headline'>Digital EAP</span>
+          <p className='paragraph'>Watch your team's emotional health improve in real time</p>
+          <ul>
+            {portfolio.map((item) => (
+              <li className='bullets'>{item.info}</li>
+            ))}
+          </ul>
+          <div className='butt'>
+            <button className='button'>GET STARTED</button>
+          </div>
+        </div>
       </div>
+      <div className='home'>
+        <div className='main'>
+          <span className='headline'>Meet our Health Counselors</span>
+          <p className='paragraph1'>{counsel}</p>
+          <div>
+            <button className='button'>GET STARTED</button>
+          </div>
+        </div>
+        <div>
+          <img src={logo} className='custom-image'/>
+        </div>
+      </div>
+
+      <div className='home'>
+        <div className='main'>
+          <span className='headline'>Why HealthCare?</span>
+            {why.map((item)=>(
+              <ul className='quest'>
+                <li className='bullets1'>{item.title}</li>
+                <li className='bullets'>{item.desc}</li>
+              </ul>
+            ))}
+        </div>
+        <div>
+          <img src={logo} alt="Idontknow" className='custom-image'/>
+        </div>
+      </div>
+
     </div>
-    <div className='home'>
-      <div>
-      <img src={logo} alt='Team Image' className='custom-image'/>
-      </div>
-      <div className='main'>
-      <span className='headline'>Digital EAP</span>
-      <p className='paragraph'>Watch your team's emotional health improve in real time</p>
-      <ul>
-        {portfolio.map((item)=>(
-         <li className='bullets'>{item.info}</li>
-        ))}
-      </ul>
-      <div className='butt'>
-        <button className='button'>GET STARTED</button>
-      </div>
-      </div>
-      
-    </div>
-    </div>
-    
+
   )
 }
 
--- src/infotext/portfolio.js
+++ src/infotext/portfolio.js
@@ -15,3 +15,24 @@ export const portfolio=[
         info:"Onsite mental health workshops."
     }
 ]
+
+export const counsel=[
+    "Saaya matches you with a suitable mental health counselor, keeping in view your background, stressors, language, and personal preferences. Saaya maintains a diverse repository of healthcare providers, and offers counseling services in over 10 languages, and engages counselors from",<b> Pakistan, Saudi Arabia, Jordan, the UAE, Bahrain, India, the United States, the UK, Kenya, and Sri Lanka.</b>
+]
+
+export const why=[
+    {
+        title:"Ease of Access",
+        desc:"Access HealthCare Services from a laptop, tablet or a smartphone"
+    },
+    {
+        title:"Affordability",
+        desc:"Care for your health without breaking your money"
+
+    },
+    {
+        title:"Customised Care",
+        desc:"Find a counselor most suited to your cultural background"
+
+    }
+]
